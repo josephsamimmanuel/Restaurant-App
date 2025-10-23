3e2355ff82536ac9ef1caeb5f2aa0db1b563ae27
josephsamimmanuel <josesamimmanuel@gmail.com>
2025-10-23 16:47:38 +0530

    Add Header component with Logo, Search, and Filter functionalities; create FilterCuisines and LogoAuthSearch subcomponents

--- src/components/ProtectedRoutes/index.jsx
+++ src/components/ProtectedRoutes/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './Header'
-import Footer from '../Footer'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
 function ProtectedRoutes({children}) {
   return (
     <div className='flex flex-col min-h-screen'>
